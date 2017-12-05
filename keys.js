2460f6c24bb4a4a320defb90d82449098022a57d
JJHowell <justinjameshowell@gmail.com>
2017-12-05 00:22:51 -0600

    Added Liri With Errors

---
+++ keys.js
@@ -0,0 +1,14 @@
+
+console.log('Keys are loaded...');
+
+exports.twitterKeys = {
+  consumer_key: 'AilQxqR7pnrXrlBeLqYo8RIFY',
+  consumer_secret: '1U9SVvCao5EmqRYm5Ge11bunzzlU7JzCAxOmUzoG6QUarzui8j',
+  access_token_key: '726836941304397825-d01mWXpb641hQqmmEaI7UaLm73b9l8R',
+  access_token_secret: 'ApsS2l9WF6YxnsbOUnvggDCdYPSFDMRS77x5vCsYAYGUs',
+}
+
+exports.spotifyKeys = {
+    id: '6e95cfb887784e47ad33b5d820b57c75',
+    secret: '4e1f2b3479404f7f9ae0f37250398268'
+};
